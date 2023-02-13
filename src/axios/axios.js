20657c40a03610508739192111a0db475b8c57c0
Test <matonikolov77@yahoo.com>
2023-02-13 18:57:19 +0100

    Trying to reemove http for URL in order to check working in Kubernetes

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
 
-    baseURL: "http://" +process.env.REACT_APP_BACKEND_URL + '/api',
+    baseURL: process.env.REACT_APP_BACKEND_URL + '/api',
     headers:{
         'Access-Control-Allow-Origin' : '*',
 
